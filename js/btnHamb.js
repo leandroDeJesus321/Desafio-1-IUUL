caaa9c6a50aaf01129132390851a882ffe756b0c
Leandro de Jesus <lean_silva100@hotmail.com>
2022-11-27 18:47:47 -0300

    5º commit, Ajustes finais na responsividade  e menu dropdown

--- js/btnHamb.js
+++ js/btnHamb.js
@@ -1,7 +1,17 @@
 // função para incrementar classe incrementar ao clicar no menu hamburguer
-
+function clickHandler() {
     var btn = document.querySelector('.menu-hamburguer');
+    
 
     btn.addEventListener('click', ()=>{
+        var dropdown = document.querySelector('.nav-bar')
         btn.classList.toggle("abrir");
+        dropdown.classList.toggle("drop");
     })
+    
+}
+
+var btn = document.getElementById("btn-list");
+
+var btn_list = btn.getElementsByTagName('a');
+
